c238b53bd6f749e0a93ad70bbacb5ae1f4ccb0dd
infinityluz <infinityluz@naver.com>
2024-05-24 00:52:49 +0900

    Spring MVC demo project add

--- HelloWorld/helloworld/src/main/java/com/javacampus/helloworld/HelloworldApplication.java
+++ HelloWorld/helloworld/src/main/java/com/javacampus/helloworld/HelloworldApplication.java
@@ -11,7 +11,7 @@ public class HelloworldApplication {
 
 	@GetMapping(value="/")
 	public String index() {
-		return "Hello World Spring Boot \n MVC 로 실행";
+		return "Hello World Spring Boot \n MVC 로 실행 \n 8089";
 	}
 
 	public static void main(String[] args) {
